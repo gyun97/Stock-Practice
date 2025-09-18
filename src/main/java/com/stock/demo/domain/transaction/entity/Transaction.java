24e1167b4f97f0e91422361722c8aace8ade0b5b
KDG <rlaehrbs9702@naver.com>
2025-09-19 05:49:56 +0900

    User 엔티티 자산 필드 삭제

--- src/main/java/com/stock/demo/domain/transaction/entity/Transaction.java
+++ src/main/java/com/stock/demo/domain/transaction/entity/Transaction.java
@@ -1,4 +1,45 @@
 package com.stock.demo.domain.transaction.entity;
 
+import com.stock.demo.domain.transaction.TransactionType;
+import jakarta.persistence.*;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.sql.Time;
+
+@Entity
+@Table(name = "transactions")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "transaction_id")
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "transaction_type", nullable = false)
+    private TransactionType type;
+
+    @Column(nullable = false)
+    private Integer price;
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
