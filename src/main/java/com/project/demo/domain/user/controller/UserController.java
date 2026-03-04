45d88b7bfee98911290dd1aa4fb8a6463bff0fba
KDG <rlaehrbs9702@naver.com>
2026-03-05 02:50:23 +0900

    fix(auth): 로그아웃 시 리프레시 토큰 쿠키 삭제 및 마이페이지 로그아웃 버튼 추가

--- src/main/java/com/project/demo/domain/user/controller/UserController.java
+++ src/main/java/com/project/demo/domain/user/controller/UserController.java
@@ -1,25 +1,33 @@
 package com.project.demo.domain.user.controller;
 
-import com.project.demo.domain.user.dto.request.PasswordUpdateRequest;
-import com.project.demo.domain.user.dto.request.UpdateUserInfoRequest;
-import com.project.demo.domain.user.dto.response.GetUserResponse;
-import com.project.demo.domain.user.entity.AuthUser;
-import com.project.demo.domain.user.service.UserService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.project.demo.common.jwt.JwtUtil;
 import com.project.demo.common.response.ApiResponse;
 import com.project.demo.domain.user.dto.request.LoginRequest;
+import com.project.demo.domain.user.dto.request.PasswordUpdateRequest;
 import com.project.demo.domain.user.dto.request.SignUpRequest;
+import com.project.demo.domain.user.dto.request.UpdateUserInfoRequest;
+import com.project.demo.domain.user.dto.response.GetUserResponse;
 import com.project.demo.domain.user.dto.response.LoginResponse;
-import com.project.demo.domain.user.dto.response.TokensResponse;
+import com.project.demo.domain.user.entity.AuthUser;
+import com.project.demo.domain.user.service.UserService;
+
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/users")
@@ -85,9 +93,12 @@ public class UserController {
      * @return [로그아웃 성공 문구]
      */
     @PostMapping("/logout")
-    public ResponseEntity<ApiResponse<String>> logout(@AuthenticationPrincipal AuthUser authUser) {
+    public ResponseEntity<ApiResponse<String>> logout(@AuthenticationPrincipal AuthUser authUser,
+            HttpServletResponse httpServletResponse) {
         // 리프레시 토큰 삭제
         userService.logout(authUser.getUserId());
+        jwtUtil.clearRefreshTokenCookie(httpServletResponse, cookieSecure, cookieDomain);
+
         log.info("로그아웃 성공!");
         return ResponseEntity.ok(ApiResponse.requestSuccess("로그아웃되었습니다."));
     }
